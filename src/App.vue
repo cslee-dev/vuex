8d16809b22c8cdd13ee662d002b0473f05659665
lcs <cslee@itmoulds.com>
2020-12-15 13:57:38 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <vs-row
+    vs-align="center"
+    vs-type="flex" vs-justify="space-around" vs-w="12">
+    <vs-col vs-type="flex" vs-w="12" vs-justify="center">{{count}}</vs-col>
+    <vs-col vs-w="12" vs-type="flex" vs-justify="center">
+      <vs-button @click="increment">Up</vs-button>
+    </vs-col>
+  </vs-row>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods : {
+    increment(){
+      this.count++;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+// Vuesax Component Framework
+import Vuesax from 'vuesax'
+import 'material-icons/iconfont/material-icons.css' //Material Icons
+import 'vuesax/dist/vuesax.css' // Vuesax
+Vue.use(Vuesax)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
